227fd275c554339eec1a96a7ae0ed12977a36631
Viknesh <sadishviknesh@gmail.com>
2026-07-27 11:19:57 +0200

    Optimisation du code

--- Backend/controllers/stuff.js
+++ Backend/controllers/stuff.js
@@ -38,6 +38,14 @@ exports.modifyBook = (req, res, next) => {
            if (book.userId != req.auth.userId) {
                res.status(401).json({ message : 'Not authorized'});
            } else {
+
+              if (req.file) {
+                   const filename = book.imageUrl.split('/images/')[1];
+                   fs.unlink(`images/${filename}`, (err) => {
+                       if (err) console.log('Erreur suppression ancienne image:', err);
+                   });
+               }
+               
                Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
                .then(() => res.status(200).json({message : 'Objet modifié!'}))
                .catch(error => res.status(401).json({ error }));
